a18f5139bedb0ead01e67eb8a38099c306d2863f
Lucas Marcolino <lucasmarcolino@id.uff.br>
2020-07-15 21:03:01 -0300

    separate app from server to make tests

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,3 @@
-import { errors } from 'celebrate';
-import cors from 'cors';
-import express, { Request, Response, NextFunction } from 'express';
-import logger from 'morgan';
-import path from 'path';
-
-import itemsRoutes from '@routes/itemsRoutes';
-import pointsRoutes from '@routes/pointsRoutes';
-
-const app = express();
-
-app.use(cors());
-app.use(logger(':method :url :status - :response-time ms'));
-app.use(express.json());
-
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
-
-app.use('/points', pointsRoutes);
-app.use('/items', itemsRoutes);
-
-app.use(errors());
-
-app.use((req, res) => res.status(404).json({ error: 'Not Found' }));
-
-app.use((err: IErrorWithStatus, req: Request, res: Response, next: NextFunction) => {
-	res.status(err.status || 500).send(err.message);
-	return next();
-});
+import app from './app';
 
 app.listen(3333);
